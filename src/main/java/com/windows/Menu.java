8c56f90ad46bb353b865f1c264ac94cfcdcb220b
redex22 <n3xtfix@gmail.com>
2022-12-09 21:47:48 -0300

    initial commit

---
+++ src/main/java/com/windows/Menu.java
@@ -0,0 +1,11 @@
+package main.java.com.windows;
+
+import javax.swing.*;
+
+public class Menu {
+    private JPanel Menu;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
