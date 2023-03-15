fe00e9b10ee6de3e3bbb4e92ebbb68440aea73a3
redex22 <n3xtfix@gmail.com>
2023-03-15 11:56:02 -0300

    Persona class now has an auto increment id

--- src/main/com/java/entities/Persona.java
+++ src/main/com/java/entities/Persona.java
@@ -3,27 +3,30 @@ package main.com.java.entities;
 import java.util.LinkedList;
 
 public class Persona {
-	private static int idPersona;
+	private static byte idCount;
+	private byte idPersona;
 	private String nombre; 
 	private String apellido;
 	private String dptoResidencia;
 	private byte cantHijos;
 	private LinkedList<Vehicles> listaVehiculo;
 	
+	public Persona() {
+		Persona.idCount++;
+		this.idPersona = idCount;
+	}
+	
 	public Persona (String nombre, String apellido, String dptoResidencia,
 					byte cantHijos, LinkedList<Vehicles> listaVehiculo) {
 		
-		this.idPersona++;
+		Persona.idCount++;
+		this.idPersona = idCount;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.dptoResidencia = dptoResidencia;
 		this.cantHijos = cantHijos;
 		this.listaVehiculo = listaVehiculo;
 	
-	}
-	
-	public void setIdPersona(int id) {
-		this.idPersona = id;
 	}
 	
 	public int getIdPersona() {
